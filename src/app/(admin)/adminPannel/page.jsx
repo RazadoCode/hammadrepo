e71f8e2f64b366ad725ad0501d6f0782f07cfbb8
HammadAhmed30 <30lazers@gmail.com>
2025-03-02 23:19:06 +0500

    Asd

--- src/app/(admin)/adminPannel/page.jsx
+++ src/app/(admin)/adminPannel/page.jsx
@@ -8,19 +8,19 @@ import { redirect } from 'next/navigation';
 
 const AdminPannel = () => {
   // const {username, password, userStatus} = useSnapshot(state)
-  const username = localStorage.getItem('username');
-      const password = localStorage.getItem('password');
-      const userStatus = localStorage.getItem('userStatus');
+  // const username = localStorage.getItem('username');
+  //     const password = localStorage.getItem('password');
+  //     const userStatus = localStorage.getItem('userStatus');
 
-      console.log(username, password, userStatus)
+  //     console.log(username, password, userStatus)
 
-  if(username===process.env.NEXT_PUBLIC_USERNAME&&password===process.env.NEXT_PUBLIC_PASSWORD && userStatus === "true"){
+  // if(username===process.env.NEXT_PUBLIC_USERNAME&&password===process.env.NEXT_PUBLIC_PASSWORD && userStatus === "true"){
 
     return <AdminDash/>
-  }
-  else{
-    redirect("/sign-in")
-  }
+  // }
+  // else{
+  //   redirect("/sign-in")
+  // }
 }
 
 export default AdminPannel
